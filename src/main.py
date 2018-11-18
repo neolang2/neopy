3f7f83072518443fc806c91acd50e365a10f5e84
Cameron Reikes <cameronreikes@gmail.com>
2018-11-18 13:54:39 -0800

    Easier unit testing and working unit tests

--- src/main.py
+++ src/main.py
@@ -2,9 +2,17 @@ import sys
 import subprocess
 import argparse
 
-if __name__ == '__main__':
+
+def parse_args(args):
     arg_parser = argparse.ArgumentParser(
         description='Compile neolang language to x86 assembly')
-    if 'test' in sys.argv:
+    arg_parser.add_argument('-t', '--test', action='store_true',
+                            help='Runs unit tests')
+    return arg_parser.parse_args(args)
+
+
+if __name__ == '__main__':
+    parsed_args = parse_args(sys.argv[1:])
+    if parsed_args.test:
         exit(subprocess.call(
             [sys.executable, '-m', 'unittest', 'discover', 'tests/']))
