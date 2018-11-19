373b086884d8f67be5b9ce6115d1f0f5d44a4131
Cameron Reikes <cameronreikes@gmail.com>
2018-11-18 17:05:21 -0800

    Change in how unit tests are ran

--- src/main.py
+++ src/main.py
@@ -1,18 +1,16 @@
 import sys
+import unittest.mock
 import subprocess
 import argparse
+
+from compiler import handler
 
 
 def parse_args(args):
     arg_parser = argparse.ArgumentParser(
         description='Compile neolang language to x86 assembly')
-    arg_parser.add_argument('-t', '--test', action='store_true',
-                            help='Runs unit tests')
     return arg_parser.parse_args(args)
 
 
 if __name__ == '__main__':
     parsed_args = parse_args(sys.argv[1:])
-    if parsed_args.test:
-        exit(subprocess.call(
-            [sys.executable, '-m', 'unittest', 'discover', 'tests/']))
