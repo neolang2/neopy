641bde246c52c747b1fc036b6c63a357cc64b453
Cameron Reikes <cameronreikes@gmail.com>
2018-11-23 15:59:32 -0800

    Update unit tests to match new language spec

--- src/tests/test_lexer.py
+++ src/tests/test_lexer.py
@@ -1,40 +1,42 @@
 import unittest
 from unittest import mock
+from compiler.handler import compile_files
 from compiler.lexer import Token, TokenKind, Lexer
 import os
 
 
-class TestArgParser(unittest.TestCase):
+class TestLexer(unittest.TestCase):
     def __get_path(self, filename):
         return os.path.join(os.path.dirname(__file__), filename)
 
-    def test_lexer_on_inputfile(self):
+    def   test_lexer_on_inputfile(self):
         path = self.__get_path("example.nl")
-        lexer = Lexer()
-        lexer.lex(filename=path)
-        expected_tokens = [Token(TokenKind.TOK_EXTERN, "extern"),
-                           Token(TokenKind.TOK_IDENTIFIER, "random"),
-                           Token(ord("("), "("),
-                           Token(TokenKind.TOK_IDENTIFIER, "x"),
-                           Token(ord(")"), ")"),
-                           Token(TokenKind.TOK_FUNC, "func"),
-                           Token(TokenKind.TOK_IDENTIFIER, "random"),
-                           Token(ord("("), "("),
-                           Token(ord(")"), ")"),
-                           Token(TokenKind.TOK_IDENTIFIER, "return"),
-                           Token(TokenKind.TOK_NUMBER, "4"),
-                           Token(TokenKind.TOK_IDENTIFIER, "x"),
-                           Token(ord("="), "="),
-                           Token(TokenKind.TOK_IDENTIFIER, "random"),
-                           Token(ord("("), "("),
-                           Token(ord(")"), ")"),
-                           Token(TokenKind.TOK_IDENTIFIER, "y"),
-                           Token(ord("="), "="),
-                           Token(TokenKind.TOK_IDENTIFIER, "x"),
-                           Token(ord("*"), "*"),
-                           Token(TokenKind.TOK_NUMBER, "2.718"),
-                           Token(TokenKind.TOK_EOF, "")
+        with open(path, "r") as f:
+            lexer = Lexer(inFile=f)
+        expected_tokens = [Token(TokenKind.KEYWORD, "extern"),
+                           Token(TokenKind.IDENTIFIER, "random"),
+                           Token(TokenKind.OPERATOR, "("),
+                           Token(TokenKind.IDENTIFIER, "x"),
+                           Token(TokenKind.OPERATOR, ")"),
+                           Token(TokenKind.KEYWORD, "func"),
+                           Token(TokenKind.IDENTIFIER, "random"),
+                           Token(TokenKind.OPERATOR, "("),
+                           Token(TokenKind.OPERATOR, ")"),
+                           Token(TokenKind.IDENTIFIER, "return"),
+                           Token(TokenKind.CONSTANT, "4"),
+                           Token(TokenKind.IDENTIFIER, "x"),
+                           Token(TokenKind.OPERATOR, "="),
+                           Token(TokenKind.IDENTIFIER, "random"),
+                           Token(TokenKind.OPERATOR, "("),
+                           Token(TokenKind.OPERATOR, ")"),
+                           Token(TokenKind.IDENTIFIER, "y"),
+                           Token(TokenKind.OPERATOR, "="),
+                           Token(TokenKind.IDENTIFIER, "x"),
+                           Token(TokenKind.OPERATOR, "*"),
+                           Token(TokenKind.CONSTANT, "2718"),
+                           Token(TokenKind.EOF, "")
                            ]
         for i in range(0, len(lexer.tokens)):
             with self.subTest((expected_tokens[i], lexer.tokens[i])):
-                self.assertTrue(expected_tokens[i] == lexer.tokens[i])
+                self.assertEqual(expected_tokens[i], lexer.tokens[i])
+                #self.assertTrue(expected_tokens[i] == lexer.tokens[i])
