34291590c28861197dde722c7404b96c33ee8908
ruwix <grantpauker@gmail.com>
2018-11-25 19:57:45 -0800

    create framework for  function grammar

--- src/compiler/parser.py
+++ src/compiler/parser.py
@@ -26,19 +26,26 @@ class Node:
 
 '''
 # varriable assignment
-function statement
+# function statement
 function decleration
 code block
 return statement
-function call
+# function call
 # math expression
 '''
 start = 'compilation_unit'
 
 
-def p_compilation_unit(p):
-    '''compilation_unit : variable_declaration'''
+def p_compilation_unit_1(p):
+    '''compilation_unit : variable_declaration
+                        | function_declaration'''
     p[0] = Node("compilation_unit", leaf=p[1])
+
+
+def p_compilation_unit_2(p):
+    '''compilation_unit : compilation_unit variable_declaration 
+                        | compilation_unit function_declaration'''
+    p[0] = Node("compilation_unit", [p[2], p[1]],None)
 
 
 def p_primary_expresssion_1(p):
@@ -108,6 +115,32 @@ def p_variable_declaration(p):
     p[0] = Node("variable_declaration", [p[1], p[2], p[4]], p[3])
 
 
+def p_function_declaration(p):
+    '''function_declaration : FUNC ID LPAREN RPAREN type code_block'''
+    p[0] = Node("function_declaration", [p[1], p[2], p[3], p[4], p[5]], p[6])
+
+
+def p_function_declaration_2(p):
+    '''function_declaration : FUNC ID LPAREN parameter_list RPAREN type code_block'''
+    p[0] = Node("function_declaration", [
+                p[1], p[2], p[3], p[4], p[5], p[6]], p[7])
+
+
+def p_parameter_list(p):
+    '''parameter_list : type ID'''
+    pass
+
+
+def p_parameter_list_2(p):
+    '''parameter_list : parameter_list COMMA parameter_list'''
+    pass
+
+
+def p_code_block(p):
+    '''code_block : LBRACE RBRACE'''
+    pass
+
+
 def p_empty(p):
     'empty : '
     pass
@@ -119,8 +152,8 @@ def p_error(p):
         print("End of File!")
         return
 
-
 # Build the grammar
+
 
 plyparser = yacc.yacc()
 
